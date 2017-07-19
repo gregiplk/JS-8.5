40663f727166288112645d8968facb649e9ef7d4
Greg Izzy <izydorek.g@gmail.com>
2017-07-19 17:13:37 +0200

    git init

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// scripts.js 8.5
+
+var 
+  	a = prompt("wpisz a"),
+	h = prompt("wpisz h"),
+	triangleArea = a*h/2;
+
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
